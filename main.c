ba584ab21d7346f4351647d701f37107bc158c05
Jordi Calafat <jcalafat97@gmail.com>
2024-05-15 18:14:58 +0200

    vm interpret and run

--- main.c
+++ main.c
@@ -12,11 +12,15 @@ int main(int argc, const char *argv[]) {
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
+  int constant1 = addConstant(&chunk, 2.3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant1, 123);
 
-  writeChunk(&chunk, OP_RETURN, 123);
   writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
+
+  interpret(&chunk);
 
   freeVM();
   freeChunk(&chunk);
